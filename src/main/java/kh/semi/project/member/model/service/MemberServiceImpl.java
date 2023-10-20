5e763a481c95cb88cb40dd313a05e9e3583e2be3
jakelee0424 <qkql006@gmail.com>
2023-10-20 17:11:19 +0900

    231020 jaekyung

--- src/main/java/kh/semi/project/member/model/service/MemberServiceImpl.java
+++ src/main/java/kh/semi/project/member/model/service/MemberServiceImpl.java
@@ -92,7 +92,9 @@ public class MemberServiceImpl implements MemberService{
 	// 프로필 이미지 설정
 	@Override
 	public int updateProfile(MultipartFile profileImage, String webPath, String filePath, Member loginMember) {
+		
 		return 0;
+	}
 
 	
 	/** 회원 탈퇴 서비스

df241fef8a7824da9ba1e4ddac09e7f44a953e7b
ahnjaeyoun <vnm1994wodbs@gmail.com>
2023-10-20 17:05:55 +0900

    2023 10 20 youn aca

--- src/main/java/kh/semi/project/community/controller/CommunityController.java
+++ src/main/java/kh/semi/project/community/controller/CommunityController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import kh.semi.project.community.model.dto.Community;
 import kh.semi.project.community.model.service.CommunityService;

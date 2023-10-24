4adbcbf9ad8ee96ab7183826804ef88758250b90
jakelee0424 <qkql006@gmail.com>
2023-10-24 17:23:15 +0900

    231024 jaekyung

--- src/main/java/kh/semi/project/manager/controller/ManagerController.java
+++ src/main/java/kh/semi/project/manager/controller/ManagerController.java
@@ -1,7 +1,9 @@
 package kh.semi.project.manager.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import kh.semi.project.manager.model.dto.QNA;
 import kh.semi.project.manager.model.service.ManagerService;
@@ -37,11 +40,13 @@ public class ManagerController {
 		return "/manager/managingAsk";
 	}
 	
+	/** 1:1 문의 조회
+	 * @param doneOrYet
+	 * @return
+	 */
 	@GetMapping("/managingAsk")
 	@ResponseBody
-	public List<QNA> QNACheck(@RequestParam String doneOrYet, 
-							  Model model
-							  ) {
+	public List<QNA> QNACheck(@RequestParam String doneOrYet) {
 		
 		List<QNA> list = new ArrayList<QNA>();
 		
@@ -56,4 +61,55 @@ public class ManagerController {
 		
 		return list;
 	}
+	
+	@GetMapping("/insert")
+	public String insert(String answerArea, int qnaNo, RedirectAttributes ra) {
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		map.put("answerArea", answerArea);
+		map.put("qnaNo", qnaNo);
+		
+		int result = service.insertAnswer(map);
+		
+		String msg = null;
+		String path = "redirect:/manager/managingQNA";
+		
+		if(result > 0) {
+			msg = "답변 제출 완료";
+		} else {
+			msg = "답변 제출 실패";
+		}
+		
+		ra.addFlashAttribute("msg", msg);
+		
+		return path;
+	}
+	
+	
+	@GetMapping("/update")
+	public String update(String answerArea, int qnaNo, RedirectAttributes ra) {
+		
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		map.put("answerArea", answerArea);
+		map.put("qnaNo", qnaNo);
+		
+		int result = service.updateAnswer(map);
+		
+		String msg = null;
+		String path = "redirect:/manager/managingQNA";
+		
+		if(result > 0) {
+			msg = "답변 수정 완료";
+		} else {
+			msg = "답변 수정 실패";
+		}
+		
+		ra.addFlashAttribute("msg", msg);
+		
+		return path;
+	}
+	
+	
 }

4928e96041984835d1dfde9d96fd5eb4fdf901c9
jakelee0424 <qkql006@gmail.com>
2023-10-25 15:34:04 +0900

    231025 jaekyung

--- src/main/webapp/resources/js/popup.js
+++ src/main/webapp/resources/js/popup.js
@@ -7,20 +7,21 @@ const contentNo = document.getElementsByClassName("contentNo");
 const checkbox = document.getElementById("checkbox");
 const likeCount = document.getElementById("likeCount");
 let temp = 0;
-let tempNum = 0;
+let temp2 = 0;
 
 for(var i = 0 ; i < content.length ; i++){
   
-  const num = contentNo[i].value
-  tempNum = num;
+  const tempNum = contentNo[i].value
 
   content[i].addEventListener('click', () => {
     
+    temp2 = tempNum;
+
     modalContainerPopup.classList.remove('hidden');
   
     likeCount.innerHTML = "";
   
-    fetch("/content/like?contentNo=" + num)
+    fetch("/content/like?contentNo=" + tempNum)
       .then(response => response.json()) 
       .then(data => {
         
@@ -43,42 +44,44 @@ for(var i = 0 ; i < content.length ; i++){
 
 }
 
-checkbox.addEventListener("click", ()=>{
+checkbox.addEventListener("click", () => {
 
   temp = parseInt(temp);
-  
-    if(memberNo == ""){
-  
-      alert("로그인 후 이용해주세요");
-      checkbox.checked = false;
-      return;
-  
-    } else{
-      fetch("/member/like?contentNo=" + num)
+
+  if (memberNo == "") {
+
+    alert("로그인 후 이용해주세요");
+    checkbox.checked = false;
+    return;
+
+  } else {
+    fetch("/member/like?contentNo=" + temp2)
       .then(response => response.text())
-      .then(count =>{
-  
-          if(count > 0){
-            likeCount.innerText = temp + 1;
-            temp = likeCount.innerText;
-          } else{
-            likeCount.innerText = temp - 1;
-            temp = likeCount.innerText;
-          }
+      .then(count => {
+
+        if (count > 0) {
+          likeCount.innerText = temp + 1;
+          temp = likeCount.innerText;
+          return;
+          
+        } else {
+          likeCount.innerText = temp - 1;
+          temp = likeCount.innerText;
+          return;
+
+        }
       })
-  
-  
-  
-  
-    }
-  
-  
-  })
+
+
+
+
+  }
+
+
+})
 
 
 closeBtnPopup.addEventListener('click', () => {
   modalContainerPopup.classList.add('hidden');
 });
-
-
 

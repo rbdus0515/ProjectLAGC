2b89d84f74af1cd6e859b9c768103d7e0b85d698
yujinjjang2 <o3odw98@gamil.com>
2023-11-01 00:23:55 +0900

    update home

--- src/main/webapp/resources/js/search.js
+++ src/main/webapp/resources/js/search.js
@@ -1,13 +1,35 @@
 // --------------- 검색창 유효성 검사 ---------------
-const searchForm = document.getElementById("searchForm");
-const inputSearch = document.getElementById("input-search");
+if(searchForm != null) {
 
-searchForm.addEventListener("submit", e => {
+	const searchForm = document.getElementById("searchForm");
+	const inputSearch = document.getElementById("input-search");
+	
+	searchForm.addEventListener("submit", e => {
+	
+		if(inputSearch.value == "") {
+			alert("검색어 입력 후 검색 버튼을 눌러주세요.");
+			e.preventDefault();
+	        return;
+		}
+	
+	})
 
-	if(inputSearch.value == "") {
-		alert("검색어 입력 후 검색 버튼을 눌러주세요.");
-		e.preventDefault();
-        return;
-	}
+};
 
-});
+// --------------- 비회원이 내 일정 추가 클릭 시, 유효성 검사 ---------------
+if(addMyPlan != null) {
+
+	const addMyPlan = document.getElementById("addMyPlan");
+	
+	addMyPlan.addEventListener("click", () => {
+	
+		if (memberNo == "") {
+
+		    alert("로그인 후 이용해주세요");
+		    return;
+	
+	  	}
+		
+	})
+
+};

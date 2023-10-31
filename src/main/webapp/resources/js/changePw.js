dc5782c67938a7391a18a62eee966d1965284282
jakelee0424 <qkql006@gmail.com>
2023-10-31 18:27:29 +0900

    231031 jaekyung

--- src/main/webapp/resources/js/changePw.js
+++ src/main/webapp/resources/js/changePw.js
@@ -1,40 +1,38 @@
+const changePw = document.getElementById("changePw");
 const newMemberPw = document.getElementById("newMemberPw");
 const newMemberPwConfirm = document.getElementById("newMemberPwConfirm");
-const changePw = document.getElementById("changePw");
 
-if(changePw != null){ // 비밀번호 변경 페이지인 경우
 
-    changePw.addEventListener("submit", e => {
+changePw.addEventListener("submit", e => {
 
-        if(newMemberPw.value.trim() == ""){
-            alert("바꿀 비밀번호를 입력해주세요");
-            e.preventDefault();
-            newMemberPw.focus();
-            return;
-        }
+    if (newMemberPw.value.trim() == "") {
+        alert("바꿀 비밀번호를 입력해주세요");
+        newMemberPw.focus();
+        e.preventDefault();
+        return;
+    }
 
-        if(newMemberPwConfirm.value.trim() == ""){
-            alert("확인 비밀번호를 입력해주세요");
-            e.preventDefault();
-            newMemberPw.focus();
-            return;
-        }
+    if (newMemberPwConfirm.value.trim() == "") {
+        alert("확인 비밀번호를 입력해주세요");
+        newMemberPw.focus();
+        e.preventDefault();
+        return;
+    }
 
-        // 비밀번호 유효성 검사
-        const regEx = /^[a-zA-Z0-9\!\@\#\-\_]{6,20}$/;
-        if(!regEx.test(newMemberPw.value)){
-            alert("비밀번호가 유효하지 않습니다");
-            e.preventDefault();
-            newMemberPw.focus();
-            return;
-        }
+    // 비밀번호 유효성 검사
+    const regEx = /^[a-zA-Z0-9\!\@\#\-\_]{6,20}$/;
+    if (!regEx.test(newMemberPw.value)) {
+        alert("비밀번호가 유효하지 않습니다");
+        newMemberPw.focus();
+        e.preventDefault();
+        return;
+    }
 
-        // 비밀번호 == 비밀번호 확인
-        if(newMemberPw.value != newMemberPwConfirm.value){
-            alert("비밀번호가 일치하지 않습니다");
-            e.preventDefault();
-            newMemberPwConfirm.focus();
-            return;
-        }
-    });
-}
+    // 비밀번호 == 비밀번호 확인
+    if (newMemberPw.value != newMemberPwConfirm.value) {
+        alert("비밀번호가 일치하지 않습니다");
+        newMemberPwConfirm.focus();
+        e.preventDefault();
+        return;
+    }
+});
